526d9977db0de737026357a09b85833a99c4a2c0
Vladimir Shpachenko <kniyzvladimir@gmail.com>
2023-06-21 15:35:36 +0300

    pervaya zdacha raboty les8

--- src/Shape.java
+++ src/Shape.java
@@ -14,3 +14,36 @@ class Quad extends Shape{
         System.out.println("Это квадрат");
     }
 }
+class Triangle extends Shape{
+    @Override
+    public void shapeName() {
+        System.out.println("Это треуголик");
+    }
+}
+class Rectangle extends Shape {
+    @Override
+    public void shapeName() {
+        System.out.println("Это прямоугольник");
+    }
+}
+class Pentagon extends Shape{
+    @Override
+    public void shapeName() {
+        System.out.println("Это пятиугольник");
+    }
+}
+class PrinterShapeName{
+    public void printer (Shape shape) {
+        shape.shapeName();
+    }
+}
+
+class TestPrintShape {
+    public static void main(String[] args) {
+        PrinterShapeName printer = new PrinterShapeName();
+        Circle c1 = new Circle();
+        Pentagon p1 = new Pentagon();
+        printer.printer(c1);
+        printer.printer(p1);
+    }
+}
